0336199656fe9b02affadb941f9096f49306c20d
TaniaMAHOUCHE <mahouchetania15@gmail.com>
2024-04-05 14:07:09 +0200

    Ajout suppression

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,20 +1,22 @@
 import { useState,useEffect } from "react";
 
-function Card({ pokemon }) {
+function Card({ pokemon, list, onDelete }) {
   const [item, setPokemon] = useState(pokemon);
   useEffect(()=>{
     setPokemon(pokemon)
   },[pokemon])
+
   return (
     <div className="flex flex-col items-center bg-white rounded-md px-2 py-2">
       <div className="border border-1 border-white rounded-md flex justify-center items-center">
         <img src={item.sprites.front_default} className="h-30 w-30" />
       </div>
-      <div className="flex flex-row gap-1 justify-center items-center">
-      <span className="px-2 bg-blue-200 rounded-md text-white font-semi-bold">{item.types[0].type.name}</span>
-      <span className="font-bold">{item.name}</span>
-      </div>
       
+      <div className="flex flex-row gap-1 justify-center items-center">
+        <span className="px-2 bg-blue-200 rounded-md text-white font-semi-bold">{item.types[0].type.name}</span>
+        <span className="font-bold">{item.name}</span>
+        <button className="bg-red-500 hover:bg-red-600" onClick={() => onDelete(item.name)}>Delete</button>
+      </div>
     </div>
   );
 }
--- src/Components/List.jsx
+++ src/Components/List.jsx
@@ -3,24 +3,45 @@ import Card from "./Card"
 
 function List({pokemons}) {
   const [pokemonsList, setPokemonsList] = useState(pokemons)
+  const [isSelected, setIsSelected] = useState(false);
+
   useEffect(()=>{
     setPokemonsList(pokemons)
   },[pokemons])
-    return (
-      <>
-     <div className="grid grid-rows-4 grid-flow-col gap-4">
-      {
-        pokemonsList.map((item,index)=>{
-          return (
-            <Card pokemon={item} key={index}/>
-             
-          )
-      })
-     
-      }
-     </div>
-      </>
-    )
+
+  function handleDelete(name) {
+    const newPokemonList = pokemonsList.filter(pokemon => pokemon.name !== name);
+    setPokemonsList(newPokemonList);
   }
+
+  function handleSelectAll() {
+    const updatedPokemonList = pokemonsList.map(pokemon => ({
+      ...pokemon,
+      selected: true
+    }));
+    setPokemonsList(updatedPokemonList);
+    setIsSelected(true);
+  }
+
+  function handleDeleteAll() {
+    const newPokemonList = pokemonsList.filter(pokemon => !pokemon.selected);
+    setPokemonsList(newPokemonList);
+    setIsSelected(false);
+  }
+
+  return (
+    <>    
+    <div>
+      <button onClick={handleSelectAll}>Sélectionner tout</button>
+      {isSelected && <button onClick={handleDeleteAll}>Supprimer tout</button>}
+      <div className="grid grid-rows-4 grid-flow-col gap-4">
+        {pokemonsList.map((pokemon, index) => (
+          <Card key={index} pokemon={pokemon} list={pokemonsList} onDelete={handleDelete} />
+        ))}
+      </div>
+    </div>
+    </>
+  )
+}
   
-  export default List
+export default List
